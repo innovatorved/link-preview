068598db8a113461974fd1bf5bc006b51f09cd6c
Ved Gupta <vedgupta@protonmail.com>
2022-08-05 20:30:06 +0530

    ui changes backend improved

--- pages/api/details.js
+++ pages/api/details.js
@@ -1,31 +1,57 @@
 import jsdom from "jsdom";
 
 export default function handler(req, res) {
-  const { urlString } = req.query;
-  const { JSDOM } = jsdom;
+  try {
+    const { urlString } = req.query;
+    const { JSDOM } = jsdom;
 
-  JSDOM.fromURL(urlString, {
-    method : 'GET'
-  }).then((dom) => {
-    const { document } = dom.window;
-    const title = document.title;
-    const metaDescription = document.querySelectorAll('meta[name]');
-    const icon = document.querySelector('link[rel=icon]').getAttribute('href');
+    JSDOM.fromURL(urlString, {
+      method: 'GET'
+    }).then((dom) => {
+      const { document } = dom.window;
+      const title = document.title;
+      const metaDescription = document.querySelectorAll('meta[name]');
+      let icon = "";
+      try {
+        icon = document.querySelector('link[rel=icon]').getAttribute('href');
+        icon = icon.startsWith("http") ? icon : `http://${dom.window.location.hostname}${icon}`;
+      } catch {
+        icon = "";
+      }
+      if (icon === "") {
+        try {
+          icon = document.querySelector('link[rel="shortcut icon"]').getAttribute('href');
+          icon = icon.startsWith("http") ? icon : `http://${dom.window.location.hostname}${icon}`;
+        } catch {
+          icon = "";
+        }
+      }
 
-    const metaJSON = {};
-    metaDescription.forEach((tag => {
-      metaJSON[tag.getAttribute("name")] = tag.getAttribute("content");
-    }))
-    const data = {
-      icon : `${dom.window.location.href}${icon}`,
-      title,
-      metaJSON,
-    }
-    res.status(200).json({ 
-      success : true, 
-      data : data
+      const metaJSON = {};
+      metaDescription.forEach((tag => {
+        metaJSON[ tag.getAttribute("name") ] = tag.getAttribute("content");
+      }))
+      const data = {
+        icon,
+        title,
+        metaJSON,
+      }
+      res.status(200).json({
+        success: true,
+        data: data
+      });
+    })
+    .catch((err)=>{
+      res.status(200).json({
+        success: false
+      });
+    })
+    return;
+  } catch (error) {
+    return res.status(200).json({
+      success: false
     });
-  });
+  }
   return;
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,47 @@
-import Head from 'next/head'
-
-import { useState, useRef } from 'react';
+import Head from "next/head";
+import { useState, useRef } from "react";
 
 export default function Home() {
+  const [flagUrl, setFlagUrl] = useState(true);
+  const [copyFlag , setCopyFlag] = useState(false);
+  const [flag, setFlag] = useState(false);
+  const [data, setData] = useState({
+    icon: "",
+    title: "",
+    metaJSON: "",
+  });
 
-  const [ flagUrl, setFlagUrl ] = useState(true);
   const inputUrl = useRef();
 
   const setInputUrlField = (e) => {
-    let urlPattern = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+    let urlPattern =
+      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
     let flag = inputUrl.current.value.match(urlPattern);
     setFlagUrl(flag !== null);
-  }
+  };
 
   const livePreviewBox = (e) => {
     e.preventDefault();
-    if(!flagUrl) return ;
+    if (!flagUrl) return;
 
     fetch(`/api/details/?urlString=${inputUrl.current.value}`, {
-        method: 'GET'
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        // console.log(response)
+        if (response.success === true) {
+          setData(response.data);
+          setFlag(true);
+          setCopyFlag(false);
+        } else {
+          setFlag(false);
+        }
       })
-      .then(response => response.json())
-      .then(response => console.log(response))
-      .catch(err => console.error(err));
+      .catch((err) => {
+        setFlag(false);
+      });
   };
-
 
   return (
     <div>
@@ -34,22 +51,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <main className="text-center md:flex md:justify-center mt-[15rem]">
-      <h1 className='font-Uchen first:head text-3xl font-bold'>Live-Preview
-        <span className='blink'>{' >'}</span>
-      </h1>
+      <main className="text-center md:flex md:justify-center mt-[10rem]">
+        <h1 className="font-Uchen text-[#411071] text-3xl font-bold">
+          Live-Preview
+          <span className="blink">{" >"}</span>
+        </h1>
 
-      <form className='mt-[3rem]' onSubmit={livePreviewBox}>
-        <label htmlFor="exampleURL0" className="font-Uchen inline-block text-xs form-label mb-2 text-gray-700"
-        >
-          Enter URL :
-        </label>
-        <br />
-        <input
-          ref={inputUrl}
-          onChange={setInputUrlField}
-          type="text"
-          className="
+        <form className="mt-[3rem]" onSubmit={livePreviewBox}>
+          <label
+            htmlFor="exampleURL0"
+            className="font-Uchen inline-block text-xs form-label mb-2 text-gray-700"
+          >
+            Enter URL :
+          </label>
+          <br />
+          <input
+            ref={inputUrl}
+            onChange={setInputUrlField}
+            type="text"
+            className="
           form-control
           w-[22rem]
           md:w-[32rem]
@@ -66,22 +86,53 @@ export default function Home() {
           m-0
           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
         "
-          id="exampleURL0"
-          placeholder="https://example.com/"
-        />
-        <br />
-        <div className='text-xs inline-block md:w-[32rem] w-[22rem]' >
-          {
-            flagUrl === true ?
-              <></> :
-              <span className='text-red-800 font-medium font-Uchen'>&#10060; {'Not in a URL format'}</span>
-          }
-        </div>
-      </form>
-
-    </main>
-
-      
+            id="exampleURL0"
+            placeholder="https://example.com/"
+          />
+          <br />
+          <div className="text-xs inline-block md:w-[32rem] w-[22rem]">
+            {flagUrl === true ? (
+              <></>
+            ) : (
+              <span className="text-red-800 font-medium font-Uchen">
+                &#10060; {"Not in a URL format"}
+              </span>
+            )}
+          </div>
+        </form>
+      </main>
+      {flag === true ? (
+          <div className="none mb-20 flex space-x-1 mt-[2rem] mx-auto text-center w-[22rem] sm:w-[32rem] md:w-[43rem]">
+            <div className="text-center p-2 w-[30%]">
+              <img className="mx-auto block" src={data.icon} alt="Logo" />
+            </div>
+            <div className="text-center w-[70%] float-right">
+              <div className="border border-neutral-800 p-3 md:px-4 ">
+                <h1 className="md:text-xl text-base font-Uchen mt-2">
+                  {data.title}
+                </h1>
+                <p className="mt-2 text-xs sm:text-base">
+                  {data.metaJSON.description || data.metaJSON.Description}
+                </p>
+                <p className="mt-1 text-right font-Uchen">
+                  {data.metaJSON.author}
+                </p>
+              </div>
+              <div className="mx-auto text-right mt-2">
+                <button 
+                className="border-blue-700 text-blue-700 font-Uchen hover:text-blue-900 border-2 rounded-lg text-xs p-1"
+                onClick={(()=>{
+                  navigator.clipboard.writeText(JSON.stringify(data.metaJSON));
+                  setCopyFlag(true);
+                })} >{copyFlag===false?"Copy Meta Information":"Copied !"}
+                </button>
+              </div>
+            </div>
+            
+          </div>
+      ) : (
+        <></>
+      )}
     </div>
-  )
+  );
 }
